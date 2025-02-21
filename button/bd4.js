9a7625d63a2ddab3760f0a986f5410afcc0e12aa
desphindj <desphindennis@gmail.com>
2025-02-21 14:59:05 +0530

    Add files via upload

---
+++ button/bd4.js
@@ -0,0 +1,12 @@
+function dj()
+{
+    let num=document.getElementById("para")
+    if(num.style.display==="none" || num.style.display==="")
+    {
+        num.style.display="block";
+    }
+    else{
+        num.style.display="none";
+    }
+}
+
